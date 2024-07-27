2a989c1d12ae03dbec9ed0321fe563c8762e974b
CamilaASantos <camilacolliaster@gmail.com>
2024-07-26 23:21:15 -0300

    Criacao de novos Tipos de dados

--- src/Main.hs
+++ src/Main.hs
@@ -4,16 +4,40 @@ module Main where
 Projeto entrega 1:
 
 [1] Criar tipos de dados voltados para o Jogo: Camila
-    Direcao;
-    Snake;
-    Pontos;
-    Comida;
-    Game;
+    Direcao; OK
+    Snake; OK
+    Posicao; OK
+    Comida; OK
+    Game; OK
 [2] Criar função de movimentação: Camila
 [3] Criar leitura do teclado
 [4] Criar Menu de seleção para o usuário
 [5] Saída do estado ("Printar" estado do jogo)
 -}
+
+-- Direções: Movimentação da cobra
+data Dir = UP | DOWN | LEFT | RIGHT deriving (Eq, Ord, Show)
+
+-- Posicao: Utilizado para mostrar a posição dos segmentos do corpo da cobra e comida
+data Pos = Int Int deriving (Eq, Ord, Show)
+
+--Comida: Posicao em que está 
+type Food = Pos
+
+--Snake: Representação da cobra no jogo
+data Snake = Snake{
+    corpo :: Pos,
+    snakeDir :: Dir,
+    state :: Int,
+    Pontos :: Int
+}
+
+--Game: Estados do jogo
+data Game = Game{
+    gameSnake :: Snake,
+    gameFood :: Food,
+    gameOver :: Bool,
+} deriving (Eq, Show)
 
 main :: IO ()
 main = do                  
