c1e8ebe0f9d9227e80efa40dc5a8b380d1c25ad3
CamilaASantos <camilacolliaster@gmail.com>
2024-07-26 22:48:46 -0300

    Commit inicial

---
+++ src/Main.hs
@@ -0,0 +1,20 @@
+module Main where
+
+{-
+Projeto entrega 1:
+
+[1] Criar tipos de dados voltados para o Jogo: Camila
+    Direcao;
+    Snake;
+    Pontos;
+    Comida;
+    Game;
+[2] Criar função de movimentação: Camila
+[3] Criar leitura do teclado
+[4] Criar Menu de seleção para o usuário
+[5] Saída do estado ("Printar" estado do jogo)
+-}
+
+main :: IO ()
+main = do                  
+  
