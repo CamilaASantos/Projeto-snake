cd75dc49d12e9f8b6a9abae1d59069ae7d5fd59e
fernanda-sayuri <fernanda@CPX-P3H7ZM37QYQ.dir.svc.accenture.com>
2024-08-05 22:42:49 -0300

    criando feature de input das teclas

--- src/Main.hs
+++ src/Main.hs
@@ -20,80 +20,101 @@ data Snake = Snake{
   speed :: Float, -- Velocidade de movimento
   state :: Bool, -- Armazena o estado da cobra ("viva" ou "morta")
   pontuacao :: Int --Armazena pontuacao
-}
+} deriving (Eq, Show)
 
 --Game: Estados do jogo
-data Game = Game{
-  gameSnake :: Snake,
-  gameFood :: Food,
-  gameOver :: Bool,
-} deriving (Eq, Show)
+data Game = Menu | Play Snake Food Bool | GameOver deriving (Eq, Show)
 
 -- Funcao de movimentacao da cobra
 movimento :: Snake -> Dir -> Snake
 movimento  = undefined
 
 --Extremidades da tela 
-limiteTela :: Num a -> a
-limiteTela = 400
+limiteTela :: Num a => a
+limiteTela = 500
 
 -- Checa se a cobra atingiu a extremidade do Tabuleiro
 checaEx :: Snake -> Snake
-checaEx cobra@(Snake ((x,y):xs) _ _ estado _ ) =
-  |x > limiteTela || x < -limiteTela  = cobra{state = False}
-  |y > limiteTela || y < -limiteTela  = cobra{state = False}
-  |otherwise = cobra
+checaEx cobra@(Snake ((x,y):_) _ _ estado _ ) =
+  if x > limiteTela || x < -limiteTela || y > limiteTela || y < -limiteTela
+  then cobra { state = False }
+  else cobra
 
--- Checa se a cobra  colidiu com seu corpo 
-checaColisao :: Snake  -> Snake
-checaColisao cobra@(Snake (x:xs) _ _ estado _ ) d = 
-  | x `elem` xs  = cobra{state = False}
-  | otherwise = cobra
+-- Checa se a cobra colidiu com seu corpo
+checaColisao :: Snake -> Snake
+checaColisao cobra@(Snake (x:xs) _ _ estado _ ) =
+  if x `elem` xs
+  then cobra { state = False }
+  else cobra
 
 -- Representação gráfica da fruta
 desenhaComida :: Food -> Picture
-desenhaComida ((x,y), img) =
-  translate x y img
+desenhaComida ((x,y), img) = translate x y img
 
 -- Função construída utilizando ChatGPT como ajuda
 selecionaAleatorio :: [a] -> IO a
 selecionaAleatorio [] = error "Lista de imagens não localizada"
 selecionaAleatorio ls = do
   let tamanho = length ls
-  indice <-randomRIO (0, tamanho - 1)
+  indice <- randomRIO (0, tamanho - 1)
   return (ls !! indice)
 
+-- Cria uma comida em uma posição aleatória
 criaComida :: StdGen -> Picture -> Food 
-criaComida g img =
-  (coords, img)
-  where 
+criaComida g img = ((x, y), img)
+  where
     (xGen, yGen) = split g
-    convCoord x y = (x * 2 * limiteTela - limiteTela , y * 2 * limiteTela - limiteTela)
-    coords = zipWith convCoord (randoms xGen) (randoms yGen)
-      
+    (x, _) = randomR (-limiteTela, limiteTela) xGen
+    (y, _) = randomR (-limiteTela, limiteTela) yGen
+
+-- Função que desenha o menu
+desenhaMenu :: Picture
+desenhaMenu = pictures [
+    translate (-100) 100 $ scale 0.5 0.5 $ color white $ text "Escolha a dificuldade:",
+    translate (-100) 50 $ scale 0.3 0.3 $ color green $ text "Pressione E para Easy",
+    translate (-100) 0 $ scale 0.3 0.3 $ color yellow $ text "Pressione M para Medium",
+    translate (-100) (-50) $ scale 0.3 0.3 $ color red $ text "Pressione H para Hard"
+  ]
+
+-- Renderiza o estado do jogo
+renderGame :: Game -> Picture
+renderGame Menu = desenhaMenu
+renderGame (Play snake food _) = pictures $ desenhaCorpo (corpo snake) ++ [desenhaComida food]
+  where
+    desenhaCorpo = map (\(x, y) -> translate x y $ color green $ rectangleSolid 20 20)
+renderGame GameOver = translate (-100) 0 $ scale 0.5 0.5 $ color red $ text "Game Over"
+
+-- Atualiza o estado do jogo
+atualizaGame :: Float -> Game -> Game
+atualizaGame _ game@(Play snake _ _) 
+  | not (state snake) = GameOver
+  | otherwise = game
+atualizaGame _ game = game
+
+-- Reage à entrada do teclado para mudar a direção da cobra ou iniciar o jogo
+handleInput :: Event -> Game -> Game
+handleInput (EventKey (Char 'e') _ _ _) Menu = iniciarJogo 1 -- Easy
+handleInput (EventKey (Char 'm') _ _ _) Menu = iniciarJogo 3 -- Medium
+handleInput (EventKey (Char 'h') _ _ _) Menu = iniciarJogo 5 -- Hard
+handleInput (EventKey (SpecialKey KeyUp) _ _ _) (Play snake food over) = Play (snake { snakeDir = UP }) food over
+handleInput (EventKey (SpecialKey KeyDown) _ _ _) (Play snake food over) = Play (snake { snakeDir = DOWN }) food over
+handleInput (EventKey (SpecialKey KeyLeft) _ _ _) (Play snake food over) = Play (snake { snakeDir = LEFT }) food over
+handleInput (EventKey (SpecialKey KeyRight) _ _ _) (Play snake food over) = Play (snake { snakeDir = RIGHT }) food over
+handleInput (EventKey (SpecialKey KeyEsc) _ _ _) game = game -- Adiciona uma condição para sair do jogo com ESC
+handleInput _ game = game
+
+-- Função para iniciar o jogo com uma determinada velocidade
+iniciarJogo :: Float -> Game
+iniciarJogo velocidade = Play (Snake [(0,0)] RIGHT velocidade True 0) comida False
+  where
+    comida = ((0, 0), color red $ circleSolid 10)
+
 main :: IO ()
 main = do
 
-  -- Carregar imagens de frutas (utilizado vídeo disponível em:https://www.youtube.com/watch?v=jtgcJrDQR8U como base para desenvolver a importação das imagens e toda parte de posicionamento das frutas)
-  let frutasArq = ["_pear.png", "_orange.png", "_watermelon.png", "_apple.png"]
-  frutas <- mapM loadPNG frutasArq
-  
-  gen <- getStdGen
-
-  let selecionaFruta = selecionaAleatorio frutas
-  img <- selecionaFruta
-  let comida = criaComida gen img
-  let cobra = Snake [(0,0)] UP 10 True 0 -- configuracoes iniciais
-
-  simulate
-    janela
-    black
-    60
-    (Game (Snake [(0,0)] RIGHT 1 True 0) comida False)
-    desenhaComida
-    renderGame
-
+  let janela = InWindow "Tabuleiro" (2 * limiteTela, 2 * limiteTela) (500, 500)
+  play janela black 10 Menu renderGame handleInput atualizaGame
+  putStrLn "Fim do jogo!"  -- Imprime ao sair
+  -- Imprime o estado do jogo ao sair (para fins de debug ou análise)
   where
-    janela = InWindow "Tabuleiro" (2 * limiteTela, 2 * limiteTela) (50,50)
-      
-
+    imprimirEstado game = putStrLn $ "Estado do jogo: " ++ show game
